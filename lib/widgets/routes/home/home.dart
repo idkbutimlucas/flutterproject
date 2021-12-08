95c6e27c99f22cf957831451c7e76aa4eabfe394
idkbutimlucas <lucasstreet11@orange.fr>
2021-12-08 16:28:02 +0100

    nutri score + food name
    add text field nutri score and food name

--- lib/widgets/routes/home/home.dart
+++ lib/widgets/routes/home/home.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -6,6 +6,28 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Food Search"),
+        ),
+        body: Center(
+            child: Column(children: const <Widget>[
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Nom du Produit',
+              hintText: 'Entrez le nom du Produit',
+              icon: Icon(Icons.no_food),
+            ),
+            keyboardType: TextInputType.text,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Nutri-Score',
+              hintText: 'Entrez la lettre du nutri-score',
+              icon: Icon(Icons.leaderboard),
+            ),
+            keyboardType: TextInputType.text,
+          ),
+        ])));
   }
 }
