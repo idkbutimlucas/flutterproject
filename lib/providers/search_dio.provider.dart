fa5394c269e522b6ead6e68737e1e248ac18338b
HJ-Jason <jason.heldebaume03@gmail.com>
2021-12-15 11:16:18 +0100

    url changes

--- lib/providers/search_dio.provider.dart
+++ lib/providers/search_dio.provider.dart
@@ -7,9 +7,10 @@ import 'package:iut2021/providers/dio.dart';
 
 final searchDioProvider = FutureProvider<Food?>((ref) async {
   final Dio dio = ref.read(dioProvider);
+  final String URLWithoutNutriscore =
+      '&tagtype_0=origins&tag_contains_0=contains&tag_0=France&tagtype_1=ingredients&tag_contains_1=contains&tag_1=chocolat&fields=id,brands,carbon_footprint_percent_of_known_ingredients,conservation_conditions_fr,product_name_fr,image_url,ingredients,labels,nutriscore_grade,origins,quantity,stores';
   try {
-    final Response response = await dio.get(
-        '&tagtype_0=origins&tag_contains_0=contains&tag_0=France&tagtype_1=ingredients&tag_contains_1=contains&tag_1=chocolat&fields=id,brands,carbon_footprint_percent_of_known_ingredients,conservation_conditions_fr,product_name_fr,image_url,ingredients,labels,nutriscore_grade,origins,quantity,stores');
+    final Response response = await dio.get(URLWithoutNutriscore);
     return Food.fromJson(response.data);
   } catch (e) {
     return Future.error(e);
