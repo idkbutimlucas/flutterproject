c82b36aa6c811a462d11311295d1e624cde48aa0
HJ-Jason <jason.heldebaume03@gmail.com>
2021-12-15 10:53:25 +0100

    search

--- lib/widgets/routes/listproduct/ProductListPage.dart
+++ lib/widgets/routes/listproduct/ProductListPage.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:iut2021/models/food.dart';
 import 'package:iut2021/models/products.dart';
 
-import '../../../providers/test_dio.provider.dart';
+import '../../../providers/search_dio.provider.dart';
 
 class ProductListPage extends ConsumerWidget {
   const ProductListPage({Key? key}) : super(key: key);
@@ -11,33 +11,35 @@ class ProductListPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-
-
-  //----------------------------------------------------------------------
+      //----------------------------------------------------------------------
       // la APPBAR
       appBar: AppBar(
         backgroundColor: Colors.white,
 
-        // titre de la page 
-        title: const Text('Resultats',
-         style: TextStyle(color: Colors.black,
-          fontFamily: 'FiraSans',
-          fontWeight: FontWeight.bold,
-          fontSize: 32, 
-         ),textAlign: TextAlign.center,
+        // titre de la page
+        title: const Text(
+          'Resultats',
+          style: TextStyle(
+            color: Colors.black,
+            fontFamily: 'FiraSans',
+            fontWeight: FontWeight.bold,
+            fontSize: 32,
+          ),
+          textAlign: TextAlign.center,
         ),
 
         // Incos de Retour
-        leading: const IconButton(alignment: Alignment.centerRight,
-           icon: Icon(Icons.arrow_back_ios),
-           tooltip: 'Retour',
-           onPressed: null, color: Colors.black
-        ),
-      ), 
+        leading: const IconButton(
+            alignment: Alignment.centerRight,
+            icon: Icon(Icons.arrow_back_ios),
+            tooltip: 'Retour',
+            onPressed: null,
+            color: Colors.black),
+      ),
 
-  // ---------------------------------------------------------------------
+      // ---------------------------------------------------------------------
       // Le corps de l'appli
-         body: ref
+      body: ref
           .watch(testDioProvider)
           .map(data: _onData, error: _onError, loading: _onLoading),
     );
@@ -58,10 +60,8 @@ class ProductListPage extends ConsumerWidget {
     );
   }
 
-
-
   // ------------------------------------------------------------------------
-  // La liste des ingrédients 
+  // La liste des ingrédients
   Widget _onData(data) {
     Food f = data.value;
     return ListView.builder(
@@ -72,53 +72,36 @@ class ProductListPage extends ConsumerWidget {
             Products p = f.products![position];
             Navigator.pushNamed(context, '/product_page', arguments: p);
           },
-
           child: Container(
             width: 100,
             height: 350,
             alignment: Alignment.center,
             padding: EdgeInsets.all(20),
             margin: EdgeInsets.all(20),
-            
+
             decoration: BoxDecoration(
-              color: Color.fromARGB(179, 196, 188, 188),
-              borderRadius: BorderRadius.circular(10)),
-              
+                color: Color.fromARGB(179, 196, 188, 188),
+                borderRadius: BorderRadius.circular(10)),
 
-          //-------------------------------------
-          // Affichage pour un produit
-            
-              child: Row(
-                children: [Column(children: [Text(
-                  f.products![position].name!,
-                  textAlign: TextAlign.center,
-                  ),
+            //-------------------------------------
+            // Affichage pour un produit
 
-                  Image.network(
-                    f.products![position].imageUrl!,
-                    width: 200,
-                    height: 250,
-                    alignment: Alignment.bottomCenter,
-                  ),
-                ]
-              
-             
-                  
-                
-                ),]
-              
+            child: Column(children: [
+              Row(children: [
+                Text(f.products![position].name!),
+                Image.network(
+                  f.products![position].imageUrl!,
+                  width: 200,
+                  height: 200,
+                ),
+              ])
+            ]),
+
+            //-------------------------------------
+            // Affichage pour un produit
           ),
-
-          //-------------------------------------
-          // Affichage pour un produit
-          
-        
-        ),
         );
-        
       },
     );
   }
 }
-
-
