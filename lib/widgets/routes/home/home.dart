fdbaa197849e597f3235ba219c9a3ba4a85139e2
idkbutimlucas <lucasstreet11@orange.fr>
2021-12-15 12:41:35 +0100

    Home page full
    home page done

--- lib/widgets/routes/home/home.dart
+++ lib/widgets/routes/home/home.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String nutriscore = "a";
+  String additif = "indiférent";
+  String palme = "indiférent";
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+
     return Scaffold(
         appBar: AppBar(
           title: const Text("Food Search"),
@@ -14,13 +24,13 @@ class Home extends StatelessWidget {
           padding: const EdgeInsets.all(10.0),
           child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: const <Widget>[
-                Text(
+              children: <Widget>[
+                const Text(
                   "Recherchez",
                   textAlign: TextAlign.center,
                   style: TextStyle(height: 2, fontSize: 25),
                 ),
-                TextField(
+                const TextField(
                   decoration: InputDecoration(
                     labelText: 'Nom du Produit',
                     hintText: 'Entrez le nom du Produit',
@@ -28,20 +38,174 @@ class Home extends StatelessWidget {
                   ),
                   keyboardType: TextInputType.text,
                 ),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Nutri-Score',
-                    hintText: 'Entrez la lettre du nutri-score',
-                    icon: Icon(Icons.leaderboard),
-                  ),
-                  keyboardType: TextInputType.text,
-                ),
-                Text(
+                Card(
+                    child: Column(
+                  children: [
+                    const Text(
+                      'Nutri-Score',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(height: 2, fontSize: 18),
+                    ),
+                    Row(
+                      children: [
+                        Radio(
+                          value: "a",
+                          groupValue: nutriscore,
+                          onChanged: nutriscoreMethod,
+                          activeColor: Colors.green[200],
+                        ),
+                        const Text("A"),
+                        Radio(
+                          value: "b",
+                          groupValue: nutriscore,
+                          onChanged: nutriscoreMethod,
+                          activeColor: Colors.green[100],
+                        ),
+                        const Text("B"),
+                        Radio(
+                          value: "c",
+                          groupValue: nutriscore,
+                          onChanged: nutriscoreMethod,
+                          activeColor: Colors.yellow,
+                        ),
+                        const Text("C"),
+                        Radio(
+                          value: "d",
+                          groupValue: nutriscore,
+                          onChanged: nutriscoreMethod,
+                          activeColor: Colors.orange,
+                        ),
+                        const Text("D"),
+                        Radio(
+                          value: "e",
+                          groupValue: nutriscore,
+                          onChanged: nutriscoreMethod,
+                          activeColor: Colors.red,
+                        ),
+                        const Text("E"),
+                      ],
+                    )
+                  ],
+                )),
+                // const TextField(
+                //   decoration: InputDecoration(
+                //     labelText: 'Nutri-Score',
+                //     hintText: 'Entrez la lettre du nutri-score',
+                //     icon: Icon(Icons.leaderboard),
+                //   ),
+                //   keyboardType: TextInputType.text,
+                // ),
+                const Text(
                   "Ingrédients",
                   textAlign: TextAlign.left,
                   style: TextStyle(height: 3, fontSize: 20),
                 ),
+                Card(
+                    child: Column(
+                  children: [
+                    const Text(
+                      'Aditiffs',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(height: 2, fontSize: 18),
+                    ),
+                    Row(
+                      children: [
+                        Radio(
+                          value: "sans",
+                          groupValue: additif,
+                          onChanged: additifMethod,
+                          activeColor: Colors.blue,
+                        ),
+                        const Text("Sans"),
+                        Radio(
+                          value: "avec",
+                          groupValue: additif,
+                          onChanged: additifMethod,
+                          activeColor: Colors.blue,
+                        ),
+                        const Text("Avec"),
+                        Radio(
+                          value: "indiférent",
+                          groupValue: additif,
+                          onChanged: additifMethod,
+                          activeColor: Colors.blue,
+                        ),
+                        const Text("Indiférent"),
+                      ],
+                    )
+                  ],
+                )),
+                Card(
+                    child: Column(
+                  children: [
+                    const Text(
+                      'Huile de palme',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(height: 2, fontSize: 18),
+                    ),
+                    Row(
+                      children: [
+                        Radio(
+                          value: "sans",
+                          groupValue: palme,
+                          onChanged: palmeMethod,
+                          activeColor: Colors.blue,
+                        ),
+                        const Text("Sans"),
+                        Radio(
+                          value: "avec",
+                          groupValue: palme,
+                          onChanged: palmeMethod,
+                          activeColor: Colors.blue,
+                        ),
+                        const Text("Avec"),
+                        Radio(
+                          value: "indiférent",
+                          groupValue: palme,
+                          onChanged: palmeMethod,
+                          activeColor: Colors.blue,
+                        ),
+                        const Text("Indiférent"),
+                      ],
+                    )
+                  ],
+                )),
+                ElevatedButton(
+                    onPressed: () {
+                      if (_dropdownFormKey.currentState!.validate()) {
+                        //valid flow
+                      }
+                    },
+                    child: const Text(
+                      "Recherchez",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(height: 1, fontSize: 20),
+                    ))
               ]),
         ));
   }
+
+  void nutriscoreMethod(value) {
+    setState(() {
+      nutriscore = value as String;
+    });
+  }
+
+  void additifMethod(value) {
+    setState(() {
+      additif = value as String;
+    });
+  }
+
+  void palmeMethod(value) {
+    setState(() {
+      palme = value as String;
+    });
+  }
+}
+
+// ignore: camel_case_types
+class _dropdownFormKey {
+  // ignore: prefer_typing_uninitialized_variables
+  static var currentState;
 }
