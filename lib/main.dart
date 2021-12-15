d1ac49d4a0e557589333cecf09669f2f3aa81c33
LacyAttorney599 <clem.blaizel@outlook.fr>
2021-12-15 12:14:45 +0100

    Update ProductListPage

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const ProviderScope(
-        child: Home(),
+        child: ProductListPage(),
       ),
       routes: {
         '/product_page': (context) => const ProductPage(),
