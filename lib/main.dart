7948b6318fb40e4492367f22d6930ca5f0e6d300
idkbutimlucas <76565606+idkbutimlucas@users.noreply.github.com>
2022-01-05 15:45:04 +0100

    title change
    .

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'flutter test',
+      title: 'flutter',
       theme: ThemeData(
         // This is the theme of your application.
         //
